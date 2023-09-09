fb005aa2c9cb497d79bb59f56c64985894bedc52
LiHiudong <245822210@qq.com>
2023-09-09 08:49:36 +0800

    新建office转换类

---
+++ src/main/java/com/bvt/encodezip/util/Office2HtmlUtils.java
@@ -0,0 +1,12 @@
+package com.bvt.encodezip.util;
+
+
+/**
+ * @projectName: EncodeZip
+ * @package: com.bvt.encodezip.util
+ * @className: Office2HtmlUtils
+ * @author: Tom
+ * @description: TODO
+ */
+public class Office2HtmlUtils {
+}
